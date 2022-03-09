b7e1501b24532bfc0627059ad389bd9c78c7c8be
JosephJ42 <josephjarvis42@gmail.com>
2022-03-09 13:24:37 +0000

    Inital implementation of logo in forms, design and implementation of UWEFlix logo

--- uweflix/forms.py
+++ uweflix/forms.py
@@ -8,3 +8,12 @@ venues= [('uwe', 'UWE')
 
 class venueForm(forms.Form):
    choseVenue = forms.CharField(label='Choose a Venue: ', widget=forms.Select(choices=venues))
+
+class loginForm(forms.Form):
+        userName = forms.CharField(max_length=12)
+        password = forms.PasswordInput(max_length=12)
+
+class signUpFormForm(forms.Form):
+        userName = forms.CharField()
+        password = forms.PasswordInput()
+
